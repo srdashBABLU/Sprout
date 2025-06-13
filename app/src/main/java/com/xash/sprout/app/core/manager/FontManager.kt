51fd680c884a59eab21e0ecfc4c3d737834143eb
srdashBABLU <srdashbablu143@gmail.com>
2025-06-13 21:50:14 +0530

    added a font manager !

---
+++ app/src/main/java/com/xash/sprout/app/core/manager/FontManager.kt
@@ -0,0 +1,32 @@
+package com.xash.sprout.app.core.manager
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.xash.sprout.R
+
+val UrbanistBold = FontFamily(
+    Font(R.font.urbanist_bold, FontWeight.Normal)
+)
+val UrbanistExtraBold = FontFamily(
+    Font(R.font.urbanist_extrabold, FontWeight.Normal)
+)
+val UrbanistExtraLight = FontFamily(
+    Font(R.font.urbanist_extralight, FontWeight.Normal)
+)
+val UrbanistLight = FontFamily(
+    Font(R.font.urbanist_light, FontWeight.Normal)
+)
+val UrbanistMedium = FontFamily(
+    Font(R.font.urbanist_medium, FontWeight.Normal),
+)
+val UrbanistRegular = FontFamily(
+    Font(R.font.urbanist_regular, FontWeight.Normal)
+)
+val UrbanistSemiBold = FontFamily(
+    Font(R.font.urbanist_semibold, FontWeight.Normal)
+)
+val UrbanistThin = FontFamily(
+    Font(R.font.urbanist_thin, FontWeight.Normal)
+)
